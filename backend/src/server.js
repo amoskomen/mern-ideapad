86f020aa34ee15813ad016e89d56824da8e63e0a
Amos Komen <kkomenamos@gmail.com>
2025-07-01 22:57:05 +0300

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,18 +4,22 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-); // Enable CORS for all routes
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  ); // Enable CORS for all routes
+}
 
 app.use(express.json()); // Parse JSON request bodies: req.body
 app.use(rateLimiter); // Apply rate limiting middleware
@@ -27,6 +31,14 @@ app.use(rateLimiter); // Apply rate limiting middleware
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
